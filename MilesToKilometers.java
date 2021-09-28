c9293a7491c229f97206a616b3c9a641817d415b
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-09-28 20:47:56 +0600

    Update MilesToKilometers.java

--- MilesToKilometers.java
+++ MilesToKilometers.java
@@ -1,18 +1,19 @@
 // Karen Nurlybekov, Nikolay Sekerbayev, Om Padhyay, Deva Narayanan
+// Program converts inputted by user length in miles to kilometers
 
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner class
 
 public class MilesToKilometers {
-    static final double MIL2KM = 1.60934;
+    static final double MIL2KM = 1.60934; // declaration of constant variable 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double inputMiles = 0.0;
-        double resulKilometers = 0.0;
+        Scanner scan = new Scanner(System.in); // Scanner object
+        double inputMiles = 0.0; // variable declaration
+        double resulKilometers = 0.0; // variable declaration
         System.out.println("***Miles to Kilometers Converter***");
         System.out.println("What the distance in Miles?");
-        inputMiles = scan.nextDouble();
-        resulKilometers = inputMiles * MIL2KM;
-        System.out.println("Converted: " + inputMiles + " miles" + " = " +resulKilometers+ " kilometers");
+        inputMiles = scan.nextDouble(); // taking user's input
+        resulKilometers = inputMiles * MIL2KM;  // calculation
+        System.out.println("Converted: " + inputMiles + " miles" + " = " +resulKilometers+ " kilometers"); // output of result
     }
 }
