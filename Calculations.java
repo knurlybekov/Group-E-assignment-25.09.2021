03498dd38ea946fc2ab99b7fe0f5e78573cb3b33
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-09-28 20:37:03 +0600

    Update Calculations.java

--- Calculations.java
+++ Calculations.java
@@ -1,40 +1,41 @@
 // Karen Nurlybekov, Nikolay Sekerbayev, Om Padhyay, Deva Narayanan
+// Program counts 2 user-entered digits with +, -, *, /, % operators
 
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner class
 
 public class Calculations {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int inputValue1 = 0;
-        int inputValue2 = 0;
-        int resultSum = 0;
-        int resultDifference = 0;
-        int resultProduct = 0;
-        int resultQuotient = 0;
-        int resultRemainder = 0;
+        Scanner scan = new Scanner (System.in); // Scanner object
+        int inputValue1 = 0; // variable declaration
+        int inputValue2 = 0; // variable declaration
+        int resultSum = 0; // variable declaration
+        int resultDifference = 0; // variable declaration
+        int resultProduct = 0; // variable declaration
+        int resultQuotient = 0; // variable declaration
+        int resultRemainder = 0; // variable declaration
 
 
         System.out.println("Please enter two values for the love of God!");
         System.out.println("And your first value will be...");
-        inputValue1 = scan.nextInt();
+        inputValue1 = scan.nextInt(); // taking user`s 1st number
         System.out.println("Interesting... What about your second value?");
-        inputValue2 = scan.nextInt();
+        inputValue2 = scan.nextInt(); // taking user`s 2nd number
 
 
-        resultSum = inputValue1 + inputValue2;
-        resultDifference = inputValue1 - inputValue2;
-        resultProduct = inputValue1 * inputValue2;
-        resultQuotient = inputValue1 / inputValue2;
-        resultRemainder = inputValue1 % inputValue2;
+        resultSum = inputValue1 + inputValue2; // calculation with +
+        resultDifference = inputValue1 - inputValue2; // calculation with -
+        resultProduct = inputValue1 * inputValue2; // calculation with *
+        resultQuotient = inputValue1 / inputValue2; // calculation with /
+        resultRemainder = inputValue1 % inputValue2; // calculation with %
 
 
         System.out.println ("The calculations for " + inputValue1 +"and " + inputValue2 + "are:") ;
-        System.out.println (inputValue1 + " + " + inputValue2 + " = " + resultSum);
-        System.out.println (inputValue1 + " - " + inputValue2 + " = " + resultDifference);
-        System.out.println (inputValue1 + " * " + inputValue2 + " = " + resultProduct);
-        System.out.println (inputValue1 + " / " + inputValue2 + " = " + resultQuotient);
-        System.out.println (inputValue1 + " % " + inputValue2 + " = " + resultRemainder);
+        System.out.println (inputValue1 + " + " + inputValue2 + " = " + resultSum); // output of + calculation
+        System.out.println (inputValue1 + " - " + inputValue2 + " = " + resultDifference); // output of - calculation   
+        System.out.println (inputValue1 + " * " + inputValue2 + " = " + resultProduct); // output of * calculation
+        System.out.println (inputValue1 + " / " + inputValue2 + " = " + resultQuotient); // output of / calculation
+        System.out.println (inputValue1 + " % " + inputValue2 + " = " + resultRemainder); // output of % calculation
     }
 }
