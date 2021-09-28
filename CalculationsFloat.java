1c086dee9550c4257dcd5b230d143eda126593f3
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-09-28 20:41:27 +0600

    Update CalculationsFloat.java

--- CalculationsFloat.java
+++ CalculationsFloat.java
@@ -1,39 +1,40 @@
 // Karen Nurlybekov, Nikolay Sekerbayev, Om Padhyay, Deva Narayanan
+// Program counts 2 user-entered digits with +, -, *, /, % operators, but the resulting values are not rounded
 
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner class
 
 public class CalculationsFloat {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        float inputValue1 = 0;
-        float inputValue2 = 0;
-        float resultSum = 0;
-        float resultDifference = 0;
-        float resultProduct = 0;
-        float resultQuotient = 0;
-        float resultRemainder = 0;
-
+        Scanner scan = new Scanner (System.in); // Scanner object
+        float inputValue1 = 0; // variable declaration
+        float inputValue2 = 0; // variable declaration
+        float resultSum = 0; // variable declaration
+        float resultDifference = 0; // variable declaration
+        float resultProduct = 0; // variable declaration
+        float resultQuotient = 0; // variable declaration
+        float resultRemainder = 0; // variable declaration
+ 
 
         System.out.println("Please enter two values for the love of God!");
         System.out.println("And your first value will be...");
-        inputValue1 = scan.nextFloat();
+        inputValue1 = scan.nextFloat(); // taking user`s 1st number
         System.out.println("Interesting... What about your second value?");
-        inputValue2 = scan.nextFloat();
+        inputValue2 = scan.nextFloat(); // taking user`s 2nd number
 
 
-        resultSum = inputValue1 + inputValue2;
-        resultDifference = inputValue1 - inputValue2;
-        resultProduct = inputValue1 * inputValue2;
-        resultQuotient = inputValue1 / inputValue2;
-        resultRemainder = inputValue1 % inputValue2;
+        resultSum = inputValue1 + inputValue2; // calculation with +
+        resultDifference = inputValue1 - inputValue2; // calculation with -
+        resultProduct = inputValue1 * inputValue2; // calculation with *
+        resultQuotient = inputValue1 / inputValue2; // calculation with /
+        resultRemainder = inputValue1 % inputValue2; // calculation with %
 
 
         System.out.println ("The calculations for " + inputValue1 +"and " + inputValue2 + "are:") ;
-        System.out.println (inputValue1 + " + " + inputValue2 + " = " + resultSum);
-        System.out.println (inputValue1 + " - " + inputValue2 + " = " + resultDifference);
-        System.out.println (inputValue1 + " * " + inputValue2 + " = " + resultProduct);
-        System.out.println (inputValue1 + " / " + inputValue2 + " = " + resultQuotient);
-        System.out.println (inputValue1 + " % " + inputValue2 + " = " + resultRemainder);
+        System.out.println (inputValue1 + " + " + inputValue2 + " = " + resultSum); // output of + calculation
+        System.out.println (inputValue1 + " - " + inputValue2 + " = " + resultDifference); // output of - calculation
+        System.out.println (inputValue1 + " * " + inputValue2 + " = " + resultProduct); // output of * calculation
+        System.out.println (inputValue1 + " / " + inputValue2 + " = " + resultQuotient); // output of / calculation
+        System.out.println (inputValue1 + " % " + inputValue2 + " = " + resultRemainder); // output of % calculation
     }
 }
