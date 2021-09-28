a0717258b27d3d46113466d6dda2c6796ef8bb7d
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-09-28 20:45:19 +0600

    Update GallonsToLitres.java

--- GallonsToLitres.java
+++ GallonsToLitres.java
@@ -1,18 +1,19 @@
 // Karen Nurlybekov, Nikolay Sekerbayev, Om Padhyay, Deva Narayanan
+// Program converts inputed by user number of gallons to litres
 
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner class
 
 public class GallonsToLitres {
-    static final double GAL2LT = 3.78541;
+    static final double GAL2LT = 3.78541; // declaration of constant variable 
     public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-    double inputGallons = 0.0;
-    double resultLitres = 0.0;
+	Scanner scan = new Scanner(System.in); // Scanner object
+    double inputGallons = 0.0; // variable declaration
+    double resultLitres = 0.0; // variable declaration
     System.out.println("***Gallons to Litres Converter***");
     System.out.println("How many gallons?");
-    inputGallons = scan.nextDouble();
-    resultLitres = inputGallons * GAL2LT;
-    System.out.println("Converted: " + inputGallons + " gal" + " = " +resultLitres+ " litre");
+    inputGallons = scan.nextDouble(); // taking user's input
+    resultLitres = inputGallons * GAL2LT; // calculation
+    System.out.println("Converted: " + inputGallons + " gal" + " = " +resultLitres+ " litre"); // output of result
     }
 }
